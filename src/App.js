00b390f5f9b4f2060c9bd70cc73c4050adccaeea
Saket Kothari <saketkothari722@gmail.com>
2022-12-29 15:14:24 +0530

    loaders - a newer way to fetch component data

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import About from './pages/About';
 import Faq from './pages/help/Faq';
 import Contact from './pages/help/Contact';
 import NotFound from './pages/NotFound';
+import Careers, { careersLoader } from './pages/careers/Careers';
 
 // layout
 import RootLayout from './layout/RootLayout';
 import HelpLayout from './layout/HelpLayout';
+import CareersLayout from './layout/CareersLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +29,11 @@ const router = createBrowserRouter(
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        {/* Before we render the Career component it's gonna render carrersLoader function */}
+        <Route index element={<Careers />} loader={careersLoader} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
---
+++ src/pages/careers/Careers.js
@@ -0,0 +1,24 @@
+import { useLoaderData, Link } from 'react-router-dom';
+
+export default function Careers() {
+  // getting the data from below function
+  const careers = useLoaderData();
+
+  return (
+    <div className="careers">
+      {careers.map((career) => (
+        <Link to="/" key={career.id}>
+          <p>{career.title}</p>
+          <p>Based in {career.location}</p>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+// Loader function
+export const careersLoader = async () => {
+  const res = await fetch('http://localhost:4000/careers');
+
+  return res.json();
+};
